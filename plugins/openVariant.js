a397956fac11dd0fc1875028248d1010a996a36e
tom <tomphill@live.co.uk>
2025-02-24 20:05:39 +0800

    tw v4

--- plugins/openVariant.js
+++
@@ -1,8 +0,0 @@
-const plugin = require("tailwindcss/plugin");
-
-const openVariant = plugin(function ({ addVariant }) {
-  addVariant("group-open", ":merge(.group).open &");
-  addVariant("peer-open", ":merge(.peer).open ~ &");
-});
-
-module.exports = openVariant;
